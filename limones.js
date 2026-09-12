2a48473013354d6611e08576b9832b104f40e1bc
Sebaxss7 <totasigjordy60@gmail.com>
2026-09-11 23:02:51 -0500

    agregamos mover a la izquierda del personaje

--- limones.js
+++ limones.js
@@ -1,2 +1,44 @@
 let canvas=document.getElementById("areaJuego");
 let ctx=canvas.getContext("2d");
+
+const   ALTURA_SUELO=20;
+const ALTURA_PERSONAJE=60;
+const ANCHO_PERSONAJE=40;
+let personajeX=canvas.width/2;
+
+function inicar(){
+    dibujarSuelo();
+    dibujarPersonaje();
+}
+function dibujarSuelo(){
+    ctx.fillStyle="#55f724bb";
+    ctx.fillRect(0,canvas.height-ALTURA_SUELO,canvas.width,ALTURA_SUELO);
+}
+function dibujarPersonaje(){
+    ctx.fillStyle="#ff0000";
+    ctx.fillRect(personajeX,canvas.height-(ALTURA_SUELO+ALTURA_PERSONAJE),ANCHO_PERSONAJE,ALTURA_PERSONAJE);
+}
+
+function moverIzquierda(){
+    personajeX=personajeX-10;
+    actualizarPantalla();
+}
+
+function moverDerecha(){
+    personajeX=personajeX+10;
+    actualizarPantalla();
+}
+
+
+
+
+
+function actualizarPantalla(){
+    limpiarCanvas();
+    dibujarSuelo();
+    dibujarPersonaje();
+
+}
+function limpiarCanvas(){
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+}
