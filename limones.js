53701628b1408efb4a156c8debf4f1d861640ff5
Sebaxss7 <totasigjordy60@gmail.com>
2026-09-22 09:16:49 -0500

    agregar colisiones

--- limones.js
+++ limones.js
@@ -4,11 +4,17 @@ let ctx=canvas.getContext("2d");
 const   ALTURA_SUELO=20;
 const ALTURA_PERSONAJE=60;
 const ANCHO_PERSONAJE=40;
+const ANCHO_LIMON=20;
+const ALTURA_LIMON=20;
 let personajeX=canvas.width/2;
+let personajeY= canvas.height-(ALTURA_SUELO+ALTURA_PERSONAJE);
+let limonX=canvas.width/2;
+let limonY=0
 
 function inicar(){
     dibujarSuelo();
     dibujarPersonaje();
+    dibujarLimon();
 }
 function dibujarSuelo(){
     ctx.fillStyle="#55f724bb";
@@ -21,16 +27,34 @@ function dibujarPersonaje(){
 function moverIzquierda(){
     personajeX=personajeX-10;
     actualizarPantalla();
+    detectarColision();
 }
 function moverDerecha(){
     personajeX=personajeX+10;
     actualizarPantalla();
+    detectarColision();
 }
 function actualizarPantalla(){
     limpiarCanvas();
     dibujarSuelo();
     dibujarPersonaje();
+    dibujarLimon();
 }
 function limpiarCanvas(){
     ctx.clearRect(0,0,canvas.width,canvas.height);
+}
+
+function dibujarLimon(){
+    ctx.fillStyle="#ffff00";
+    ctx.fillRect(limonX,limonY,ANCHO_LIMON,ALTURA_LIMON);
+}
+
+function bajarLimon(){
+    limonY=limonY+5;
+    actualizarPantalla();
+}
+
+function detectarColision(){
+    if(limonX+ANCHO_LIMON>personajeX && limonX<personajeX+ANCHO_PERSONAJE && limonY<personajeY+ALTURA_PERSONAJE && limonY+ALTURA_LIMON>personajeY)
+        alert("ATRAPADO");
 }
